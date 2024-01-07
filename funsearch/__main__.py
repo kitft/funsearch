b56ebb469981b0a6b0659e89c9bbcc8cb34143d5
Johannes Aalto <aalto.johannes@gmail.com>
2024-01-07 22:54:09 +0200

    Implemement generic way to provide input data

--- funsearch/__main__.py
+++ funsearch/__main__.py
@@ -1,31 +1,66 @@
+import json
 import logging
 import os
 import pathlib
-import sys
+import pickle
 import time
-from pathlib import Path
 
 import click
 import llm
 from dotenv import load_dotenv
 
-sys.path.append(str(Path(__file__).parent.parent))
 
 from funsearch import config, core, sandbox, sampler, programs_database, code_manipulation, evaluator
-
 
 LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO').upper()
 logging.basicConfig(level=LOGLEVEL)
 
 
+def parse_input(filename_or_data: str):
+  if len(filename_or_data) == 0:
+    raise Exception("No input data specified")
+  p = pathlib.Path(filename_or_data)
+  if p.exists():
+    if p.name.endswith(".json"):
+      return json.load(open(filename_or_data, "r"))
+    if p.name.endswith(".pickle"):
+      return pickle.load(open(filename_or_data, "rb"))
+    raise Exception("Unknown file format or filename")
+  if "," not in filename_or_data:
+    data = [filename_or_data]
+  else:
+    data = filename_or_data.split(",")
+  if data[0].isnumeric():
+    f = int if data[0].isdecimal() else float
+    data = [f(v) for v in data]
+  return data
+
+
 @click.command()
 @click.argument("spec_file", type=click.File("r"))
+@click.argument('inputs')
 @click.option('--model_name', default="gpt-3.5-turbo-instruct", help='LLM model')
 @click.option('--output_path', default="./data/", type=click.Path(file_okay=False), help='path for logs and data')
 @click.option('--load_backup', default=None, type=click.File("rb"), help='Use existing program database')
 @click.option('--iterations', default=-1, type=click.INT, help='Max iterations per sampler')
 @click.option('--samplers', default=15, type=click.INT, help='Samplers')
-def main(spec_file, model_name, output_path, load_backup, iterations, samplers):
+def main(spec_file, inputs, model_name, output_path, load_backup, iterations, samplers):
+  """ Execute function-search algorithm:
+
+\b
+  SPEC_FILE is a python module that provides the basis of the LLM prompt as
+            well as the evaluation metric.
+            See examples/cap_set_spec.py for an example.\n
+\b
+  INPUTS    input filename ending in .json or .pickle, or a comma-separated
+            input data. The files are expected contain a list with at least
+            one element. Elements shall be passed to the solve() method
+            one by one. Examples
+              8
+              8,9,10
+              ./examples/cap_set_input_data.json
+"""
+
   # Load environment variables from .env file.
   #
   # Using OpenAI APIs with 'llm' package requires setting the variable
@@ -33,11 +68,8 @@ def main(spec_file, model_name, output_path, load_backup, iterations, samplers):
   # See 'llm' package on how to use other providers.
   load_dotenv()
 
-  specification = spec_file.read()
-
-  timestamp = int(time.time())
-
-  log_path = pathlib.Path(output_path) / str(timestamp)
+  timestamp = str(int(time.time()))
+  log_path = pathlib.Path(output_path) / timestamp
   if not log_path.exists():
     log_path.mkdir(parents=True)
     logging.info(f"Writing logs to {log_path}")
@@ -46,6 +78,7 @@ def main(spec_file, model_name, output_path, load_backup, iterations, samplers):
   model.key = model.get_key()
   lm = sampler.LLM(2, model, log_path)
 
+  specification = spec_file.read()
   function_to_evolve, function_to_run = core._extract_function_names(specification)
   template = code_manipulation.text_to_program(specification)
 
@@ -55,7 +88,7 @@ def main(spec_file, model_name, output_path, load_backup, iterations, samplers):
   if load_backup:
     database.load(load_backup)
 
-  inputs = [4]
+  inputs = parse_input(inputs)
 
   evaluators = [evaluator.Evaluator(
     database,
--- tests/main_test.py
+++ tests/main_test.py
@@ -6,18 +6,18 @@ from unittest.mock import patch
 
 from click.testing import CliRunner
 
-from funsearch.__main__ import main
+from funsearch.__main__ import main, parse_input
 
 ROOT_DIR = Path(__file__).parent.parent
 
 runner = CliRunner()
 
 
-class TestStringMethods(unittest.TestCase):
+class TestMain(unittest.TestCase):
   def setUp(self):
     self.temp_dir = tempfile.mkdtemp()
     self.default_args = ["--output_path", self.temp_dir, "--samplers", "1", "--iterations", "1",
-                         str(ROOT_DIR / "examples" / "cap_set_spec.py")]
+                         str(ROOT_DIR / "examples" / "cap_set_spec.py"), "5"]
 
   def tearDown(self):
     shutil.rmtree(self.temp_dir)
@@ -27,7 +27,7 @@ class TestStringMethods(unittest.TestCase):
     assert result.exit_code != 0
     assert 'Usage:' in result.output
     with patch('funsearch.core.run', return_value=None) as mock_run:
-      result = runner.invoke(main, self.default_args )
+      result = runner.invoke(main, self.default_args)
       assert result.exit_code == 0
       assert mock_run.call_count == 1
 
@@ -36,3 +36,9 @@ class TestStringMethods(unittest.TestCase):
       result = runner.invoke(main, self.default_args)
       assert result.exit_code == 0
       assert mock_run.call_count == 2, "There should be 2 sampler per sampler by default"
+
+
+def test_parse_input():
+  assert parse_input("1") == [1]
+  assert parse_input("1,2,3") == [1, 2, 3]
+  assert parse_input(str(ROOT_DIR / "examples" / "cap_set_input_data.json")) == [8, 9, 10]
