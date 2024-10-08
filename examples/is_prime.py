45b75ec4bf893a1b5f7e1e9bc780e18edb61adc0
kitft <58341426+kitft@users.noreply.github.com>
2024-10-08 16:12:37 -0400

    ]50;SetProfile=LLM Implement is_prime function and update evaluate to compare with priority ]50;SetProfile=Default

--- examples/is_prime.py
+++ examples/is_prime.py
@@ -1,5 +1,7 @@
 """
-On every iteration, improve priority_vX+1 over the priority_vX methods from previous iterations.
+
+
+On every iteration, improve priority_v1 over the priority_vX methods from previous iterations.
 Make only small changes.
 Try to make the code short.
 """
@@ -11,29 +13,22 @@ import funsearch
 
 import math
 
+def is_prime(n):
+    n=n-2
+    if n <= 1:
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
 
 @funsearch.run
-def evaluate(n: int) -> int:
-    """Returns the size of a certain `n`-dimensional cap set."""
-    #capset = solve(n)
-    #result = np.mean(np.equal(np.vectorize(solve)(np.arange(2, n)), np.vectorize(priority)(np.arange(2, n))))
-    result=0
-    return result
-
-
-def solve(n):
-    # if n <= 3:
-    #     return False
-    
-    # def is_prime(n):
-    #     if n <= 1:
-    #         return False
-    #     for i in range(2, int(math.sqrt(n)) + 1):
-    #         if n % i == 0:
-    #             return False
-    #     return True
-    return False#is_prime(n - 2)
-
+def evaluate(n: int) -> float:
+  """Returns the size of a certain `n`-dimensional cap set."""
+  #capset = solve(n)
+  result = np.mean(np.equal(np.vectorize(is_prime)(np.arange(5, n)), np.vectorize(priority)(np.arange(5, n))))
+  return result
 
 
 @funsearch.evolve
