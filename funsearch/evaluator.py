6d0ea7e0ad6c24a59afc14854ec032140f406cdd
Thomas Harvey <thomas.harvey@live.co.uk>
2025-01-16 09:26:48 -0500

    Allow collections and random

--- funsearch/evaluator.py
+++ funsearch/evaluator.py
@@ -37,7 +37,7 @@ from funsearch import logging_stats
 METHOD_MATCHER = re.compile(r"def priority_v\d\(.*?\)(?:\s*->.*?)?:(?:\s*(?:[ \t]*(?!def|#|`|').*(?:\n|$)))+")
 METHOD_NAME_MATCHER = re.compile(r"priority_v\d+")
 
-ALLOWED_FUNCTIONS = {'itertools', 'numpy', 'np', 'math', 'functools'}
+ALLOWED_FUNCTIONS = {'itertools', 'numpy', 'np', 'math', 'functools', 'collections', 'random'}
 DISALLOWED = { '__import__(', 'breakpoint(', 'compile(', 'open(', 'dir(', 'eval(', 'exec(', 'globals(',
               'input(', 'repr(', 'savetxt(', 'loadtxt(', 'genfromtxt(', 'fromfile(', 'tofile(', 'frombuffer(',
               'save(', 'savez(', 'savez_compressed(', 'load(', 'savetxtas', 'loadtxtas', 'genfromtxtas', 
